63aed73e9b533a627f47e57d4042375b46aec946
Tiago <tiagoletras45@gmail.com>
2020-01-01 20:35:07 -0300

    Render a <MusicList /> in <App />.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import MusicList from './Components/MusicList';
+import api from './services/api';
 
 const App: React.FC = () => {
+  const [musicList, setMusicList] = useState();
+
+  useEffect(() => {
+    console.log('useEffect');
+
+    const data = api.getMusicList();
+    setMusicList(data);
+  }, []);
+
+  console.log('rendering <App />');
   return (
     <div className="App">
       <header className="App-header">
         <p>Music App</p>
+        <MusicList musicList={musicList} />
       </header>
     </div>
   );
