29b3ffd1c09c89103803b774c680e2d8cd2bb8cb
Tiago <tiagoletras45@gmail.com>
2020-01-02 13:53:04 -0300

    Fix <Link />.

--- src/Components/MusicListItem/index.tsx
+++ src/Components/MusicListItem/index.tsx
@@ -5,13 +5,12 @@ import { Music } from '../../services/api/musics/types';
 
 interface IProps {
   music: Music;
-  i: number;
 }
 
-const MusicListItem: React.FC<IProps> = ({ music, i }) => (
-  <ListItem key={i}>
+const MusicListItem: React.FC<IProps> = ({ music }) => (
+  <ListItem>
     <ListItemText primary={music.title} secondary={music.artist} />
-    <Link href={music.about}>About</Link>
+    <Link href={music.video}>About</Link>
   </ListItem>
 );
 
